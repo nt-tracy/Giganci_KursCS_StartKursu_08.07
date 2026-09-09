62f409b243e67fe0b7f96ca7bb788ee9f28f256d
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-09-09 10:32:11 +0200

    Notes from Lesson9

---
+++ Lekcja9/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Lekcja9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string poprawne = "123";
+
+        // Console.WriteLine("Podaj hasło: ");
+        // string podane = Console.ReadLine();
+
+        // if (podane == poprawne) 
+        //     Console.WriteLine("Zalogowano");
+        // else 
+        //     Console.WriteLine("Złe hasło");
+
+
+        //while 
+        // while (warunek)
+        //{
+        //}
+
+
+        // while (poprawne != podane)
+        // {
+        //     Console.WriteLine("Podano złe hasło, spróbuj ponownie");
+        //     Console.WriteLine("Podaj hasło: ");
+        //     podane = Console.ReadLine();
+        // }
+
+        // Console.WriteLine("Zalogowano");
+
+        // while (...);
+
+        // int pozycja = 0;
+        // Random r = new Random();
+
+        // while (pozycja < 32)
+        // {
+        //     Console.WriteLine("Stoisz na pozycji " + pozycja);
+        //     Console.WriteLine("Naciśnij enter, aby rzucić kością");
+        //     Console.ReadLine();
+
+        //     int rzut = r.Next(1,7);
+        //     pozycja += rzut;
+        //     Console.WriteLine($"Idziesz {rzut} do przodu");
+
+        // }
+        // Console.WriteLine("Doszedłeś do celu");
+
+
+
+        //for(){}
+
+        //for(odkąd; dokąd; co ma się dziać)
+
+        // for (int i=10; i>0; i--)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+
+
+        while(true)
+        {
+            Console.Write("Podaj wiadomość: ");
+            string msg = Console.ReadLine();
+            
+            if(msg == "koniec")
+                continue;
+                
+            Console.WriteLine($"Wiadomość: {msg}");
+        }
+
+
+        // for(int i=1; i < 20; i++)
+        // {
+        //     if(i > 7)
+        //         continue;
+        //     Console.WriteLine(i);
+        // }
+
+
+    }
+}
