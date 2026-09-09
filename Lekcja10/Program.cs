a4ca7b2cd090bfeec3d360ba6effe6e44785bed6
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-09-09 10:41:54 +0200

    Created template for Lesson10

---
+++ Lekcja10/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Lekcja10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //#1 Zadanie rozgrzewkowe: 
+        // Stwórz program, który za pomocą pętli wyświetli 7 kolejnych potęg liczby 2.
+        // (skorzystaj z odpowiedniej pętli i zmiennej przechowującej aktualną liczbę)
+
+
+
+
+        //#2 Poznajemy działanie tablic:
+        //składnia:
+
+        //dostęp do elementów:
+
+        //System.IndexOutOfRangeException
+
+
+
+
+
+        //#3 Typy danych w tablicach:
+
+
+
+        //#4 Zadanie 1: 
+        // Korzystając z poznanych informacji o tablicach spróbuj stworzyć tablicę napisów, 
+        // która będzie zawierała twoje ulubione gry, filmy lub postaci.
+
+
+        //#5 Dynamiczne tworzenie tablic - new typ[rozmiar] 
+        // domyślne wartości dla int[] - 
+        // domyślne wartości dla string[] -
+
+
+
+        //#6 Odczytywanie rozmiaru tablicy - Length
+
+
+        //#7 Zadanie 2: 
+        // Wróć do zadania rozgrzewkowego (z potęgami). 
+        // Spróbuj umieścić wyniki w tablicy.
+
+
+
+        //#8 String jako tablica?
+
+    }
+}
