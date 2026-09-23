8fe826bb1da58af043020d9bd6a9179a53254d22
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-09-23 15:10:47 +0200

    Template for Lesson12

---
+++ Lekcja12/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Lekcja12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //#1 Zadanie 1 - Bohater gry:  Stwórz prosty program, 
+        // w którym wykorzystując zmienne zdefiniujesz podstawowe informacje o bohaterze gry RPG, 
+        // a następnie wypiszesz je na ekranie w formie spójnego komunikatu. 
+        // Zdefiniuj: nick, maksymalny i aktualny pasek życia siłę, zręczność i inteligencję. 
+
+        //Miejsce na dane bohatera:
+
+
+        // Wyświetlenie informacji: (usuń znacznik komentarza po podaniu danych wyżej)
+        // Console.WriteLine("=== KARTA POSTACI RPG ===");
+        // Console.WriteLine($"Nick: {nick}");
+        // Console.WriteLine($"Życie: {currentHp} / {maxHp}");
+        // Console.WriteLine($"Siła: {strength}");
+        // Console.WriteLine($"Zręczność: {agility}");
+        // Console.WriteLine($"Inteligencja: {intelligence}");
+
+
+
+
+
+
+        //#2 Składnia metod:
+
+
+        // static void Main()
+        // {
+        // //wywołanie własnej metody
+        // NazwaWCzasowniku();
+        // }
+
+
+        // //Własna metoda
+        // static void NazwaWCzasowniku()
+        // {
+        // //kod
+        // }
+
+
+
+        // #3 Przekazywanie argumentów do metod
+
+
+
+        // #4 Zadanie 2 - Bohaterowie gry:  
+        // Zmodyfikuj stworzony wcześniej program, aby wykorzystywał metodę do tworzenia bohatera, 
+        // a następnie stwórz dwóch nowych bohaterów. 
+
+
+
+        // #5 Zwracanie danych przez metody
+
+
+
+      
+        // #6 Zadanie 3 - Bohaterowie gry i ich moc magiczna:  
+        // Zmodyfikuj stworzony wcześniej program dodając bohaterom manę. 
+        // Wynik procentowy hp i many obliczaj metodą.  
+
+
+        // #7 Przeciążanie metod:
+
+
+
+
+        // #8 Zadanie 4 - Atakowanie wroga:  
+        // Napisz prosty symulator atakowania wroga z wykorzystaniem różnych ataków. 
+        // Stwórz przynajmniej 3 typy ataków. 
+        
+        // Dodatkowe: Skorzystaj z Enum do rozróżnienia typów jednostek.  
+        // Dodatkowe: Popraw czytelność kodu tworząc metody pomocnicze.
+
+
+
+
+    }
+
+
+}
